c8551fd6bfe47abfde1df6244413102a4f0ab139
Antonio De León <antjz@duck.com>
2025-09-14 15:49:23 -0500

    Corregido el código para traer más resultados, mejorada la interfaz.

--- Trabajo 2p - Página con consumo de APIs/script.js
+++ Trabajo 2p - Página con consumo de APIs/script.js
@@ -10,17 +10,34 @@ const search = (searchQuery) => {
   const URL = `${baseURL}${endpoint}`;
   fetch(URL)
     .then((response) => response.json())
-    .then((data) => placeInfo(data));
+    .then((data) => placeInfo(data))
+    .catch((error) => {
+      let message = document.getElementById('status');
+      message.innerText = 'No se han encontrado resultados... Prueba otra búsqueda.';
+    });
 };
 
 const placeInfo = (response) => {
   let stuff = document.getElementById('response');
-  stuff.innerHTML = `
-        <div>
-        <h3>${response[0].name}</h3>
-        <p>${response[0].country}</p>
+  response.forEach((element) => {
+    stuff.innerHTML += `
+        <div class="item">
+          <div>
+            <h3>${element.name}</h3>
+            <p>${element.country}</p>
+          </div>
+        <a class="custom-link" href="${element.web_pages[0]}">
+          <button class="custom-button">Entrar</button>
+        </a>
         </div>
-        <a href="${response[0].web_pages[0]}">
-        <button class="custom-button">Entrar</button>
-        </a>`;
+        `;
+  });
+  //   stuff.innerHTML = `
+  //         <div>
+  //         <h3>${response[0].name}</h3>
+  //         <p>${response[0].country}</p>
+  //         </div>
+  //         <a href="${response[0].web_pages[0]}">
+  //         <button class="custom-button">Entrar</button>
+  //         </a>`;
 };
